5c4811573c8b16c22c8f53d4a4d29b5b443d39e5
Monique Van Den Berg <moniquevdb109@gmail.com>
2025-11-01 18:16:59 +0200

    created a function to create task elements

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ function clearExistingTasks() {
     document.querySelectorAll('.task-container').forEach(c => c.innerHTML = '');
 }
 
+// 2. render tasks to the columns based on their status
+function createTaskElement(task) {
+    const div = document.createElemennt('div');
+    div.className = 'task-div';
+    div.textContent = task.title;
+    div.dataset.taskId = taskId
+    div.addEventListener('click',() => openTaskModal(task));
+    return div;
+}
+
